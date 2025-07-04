0a050f8703b67ecc43bd1589704f888bb7893d15
Rodrigo <lucianorodrigocarvalho@gmail.com>
2025-07-04 07:29:51 -0300

    first commit

---
+++ src/test/java/com/smartprocess/integrations/IntegrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartprocess.integrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
